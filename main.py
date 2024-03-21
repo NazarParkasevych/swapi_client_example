5a6e4438387253a6b9e16c91cb0c34e51c35ee2d
Parkasevych Nazar <parkasevychnazarij@gmail.com>
2024-03-21 10:08:01 +0200

    Get film by id. Print film.tittle

--- main.py
+++ main.py
@@ -2,14 +2,13 @@
 from swapi import swapi, get_person
 
 
-def get_films_info():
-    luke = swapi.get_film(id)
-    print (f"Фільм:(film.tittle)")
+def get_films_info(film_id):
+    film = swapi.get_film(film_id)
+    print (f"Фільм:{film.title}")
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     film_id = input('введіть ідентифікатор фільму:')
-    get_films_info()
     get_films_info(film_id)
 
