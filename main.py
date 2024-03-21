84835558aa3fcad42452fc3682adfd02047ef707
Parkasevych Nazar <parkasevychnazarij@gmail.com>
2024-03-21 10:06:20 +0200

    Get film by id. Print film.tittle

--- main.py
+++ main.py
@@ -1,16 +1,15 @@
-# This is a sample Python script.
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+from swapi import swapi, get_person
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def get_films_info():
+    luke = swapi.get_film(id)
+    print (f"Фільм:(film.tittle)")
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    film_id = input('введіть ідентифікатор фільму:')
+    get_films_info()
+    get_films_info(film_id)
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
